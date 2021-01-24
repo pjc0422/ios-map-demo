a1a4a54db84cf11c74011c42271354269c55c72c
pjc <park.jc@kt.com>
2021-01-24 22:19:46 +0900

    project structure init

--- demo/demo/ViewController.swift
+++ demo/demo/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var demoListView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        demoListView.dataSource = self
+        demoListView.delegate = self
+        demoListView.estimatedRowHeight = 55
+        demoListView.rowHeight = UITableView.automaticDimension
+
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DemoCell", for: indexPath) as! DemoCell
+        cell.model = DemoTestModel(title: "Test1", description: "hello world", viewController: nil)
+        return cell
+    }
+
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 100
+//    }
+}
+
+extension ViewController:UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("section : \(indexPath.section) ,row : \(indexPath.row)")
+    }
+}
---
+++ demo/demo/base/DemoTestModel.swift
@@ -0,0 +1,16 @@
+//
+//  DemoTestModel.swift
+//  demo
+//
+//  Created by 박주철 on 2021/01/24.
+//  Copyright © 2021 박주철. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct DemoTestModel {
+    var title: String
+    var description: String?
+    var viewController: UIViewController?
+}
